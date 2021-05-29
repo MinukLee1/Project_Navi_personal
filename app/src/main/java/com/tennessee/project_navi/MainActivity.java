25d6a7cabb75577493a4b3870f544e4e1ef4a0c2
jhsun <wngus2991@gmail.com>
2021-05-29 21:26:16 +0900

    HashKey_JH

--- app/src/main/java/com/tennessee/project_navi/MainActivity.java
+++ app/src/main/java/com/tennessee/project_navi/MainActivity.java
@@ -2,27 +2,47 @@ package com.tennessee.project_navi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapView;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "KeyHash";
 
     MapView mapView;
     RelativeLayout mapViewContainer;
 
-    Button btntest,btnboard,btnuser,btnbmark,btnlogout;
+    Button btntest,btnboard,btnuser,btnbmark,
+            btnlocation, btnlogout;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+        //가져와!
+        getAppKeyHash();
 
 
         // Initialize Firebase Auth : 회원가입이 안된상태일시 JoinActivity
@@ -30,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
             startLoginActivity();
 
         }
+
 
         mapViewContainer = findViewById(R.id.map_view);
         mapView = new MapView(this);
@@ -39,7 +60,28 @@ public class MainActivity extends AppCompatActivity {
         btnboard = findViewById(R.id.btnboard);
         btnuser = findViewById(R.id.btnuser);
         btnbmark = findViewById(R.id.btnbmark);
+        //위치 켜기 끄기
+        btnlocation = findViewById(R.id.btnlocation);
         btnlogout = findViewById(R.id.btnlogout);
+
+
+
+
+        //현재 위치 가져오기
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+
+        //현재 위치에 마커 찍어주기
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        //marker.setMapPoint(MARKER_POINT);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin);
+        //marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin);
+        mapView.addPOIItem(marker);
+
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode
+                .TrackingModeOnWithHeading);
+
 
         btntest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,7 +124,32 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
     }
+
+
+    // 해쉬키 코드
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("name not found", e.toString());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 
     private void startLoginActivity(){
         Intent intent = new Intent(this,LoginActivity.class);
